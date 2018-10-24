d4fbe27155fde3771c15df5168562a3d287eea3c
Roman Pirag <mr.anadale@yahoo.com>
2018-10-24 05:34:51 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,5 +44,5 @@ def using_delete(instructors, no_offense_steven)
 end
 
 def using_delete_at (famous_robots, int)
-  deleted_robot = famous_robots.delete(int)
+  deleted_robot = famous_robots.delete_at(int)
 end
