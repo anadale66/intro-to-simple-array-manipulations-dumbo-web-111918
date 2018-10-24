8c762c59be74ff15f7ae92e11e5407ad4a7e8d2c
Roman Pirag <mr.anadale@yahoo.com>
2018-10-24 04:56:40 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,6 @@ def using_concat(my_favorite_things, more_favs)
   all_my_favs = my_favorite_things += more_favs
 end
 
-def using_insert(list_of_esoteric_programming_languages, )
+def using_insert(list_of_esoteric_programming_languages,another_esoteric_language)
+  
 end
