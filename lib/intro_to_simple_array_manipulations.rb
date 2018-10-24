f825463bcd921e7781cf744fee13aeb11a3b5eb2
Roman Pirag <mr.anadale@yahoo.com>
2018-10-24 04:47:25 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,8 +23,7 @@ def shift_with_args(ice_cream_brands)
     brands_removed = ice_cream_brands.shift(2) 
 end
 
-def using_concat(my_favorite_things,more_favs) 
-  my_favorite_things += more_favs
-  my_favorite_things
+def using_concat(my_favorite_things, more_favs) 
+  all_my_favs = my_favorite_things += more_favs
 end
 
