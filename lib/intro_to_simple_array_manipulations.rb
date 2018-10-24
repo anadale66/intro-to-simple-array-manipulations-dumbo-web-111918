809dbb0218f507ef633033708d2a97604c0abf61
Roman Pirag <mr.anadale@yahoo.com>
2018-10-24 05:04:54 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,7 +31,7 @@ def using_insert(list_of_esoteric_programming_languages,another_esoteric_languag
   list_of_esoteric_programming_languages.insert(4,another_esoteric_language)
 end
 
-def using_uniq()
-
+def using_uniq(captain_planet_and_the_planeteers)
+  
 
 end
